4e61d35e7423065dfec02094550e203e87870fc5
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2026-03-28 22:25:49 +0530

    Time: 11 ms (52.94%), Space: 45.9 MB (96.74%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        
+        int i = 0;
+        int maxFreq = 0;
+        int maxLength = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            char ch = s.charAt(j);
+            freq[ch - 'A']++;
+
+            maxFreq = Math.max(maxFreq, freq[ch - 'A']);
+
+            // invalid window
+            if ((j - i + 1) - maxFreq > k) {
+                freq[s.charAt(i) - 'A']--;
+                i++;
+            }
+
+            maxLength = Math.max(maxLength, j - i + 1);
+        }
+
+        return maxLength;
+    }
+}
