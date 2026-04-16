c2ab15e99f30730b23c60ebf419250c9ee49ab63
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2026-04-16 23:42:08 +0530

    Time: 45 ms (26.01%), Space: 47.1 MB (62.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int indI=-1;
-        int indJ=-1;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(target==nums[i]+nums[j]){
-                    indI=i;
-                    indJ=j;
-                    break;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
                 }
-                    
             }
         }
-        return new int[]{indI,indJ};
+
+        // Required return (even if not used in LeetCode)
+        return new int[]{-1, -1};
     }
 }
