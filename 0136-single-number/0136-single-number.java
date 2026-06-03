0c3c1a238ff9707f930f3a0b31193bd35dadee06
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2026-06-03 13:43:24 +0530

    Time: 356 ms (6.71%), Space: 46.9 MB (44.69%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        int freq;
+        boolean flag = true;
         for (int i = 0; i < nums.length; i++) {
-            int freq = 0; // reset here
-
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
+            freq = 0;
+            for (int j = 0; j < nums.length ; j++) {
+                if (nums[i] == nums[j] && i !=j) {
                     freq++;
                 }
-            }
 
-            if (freq == 1) {
-                return nums[i];
             }
+            if (freq == 0)
+                return nums[i];
+
         }
         return -1;
     }
